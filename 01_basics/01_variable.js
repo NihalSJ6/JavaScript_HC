bb7e1e71a3a28756c89c64689103b4f7e5239ce8
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-06-23 06:06:24 +0000

    commit change

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountID = 13454
+let account_email = "nsj@cloud.com"
+var account_Password = "12343"
+account_City = "HongKong"
+
+let accountState;
+
+account_email = "nj_nj@gmail.com"
+account_Password = "678765"
+account_City = "Pune"
+
+console.table([accountID, account_email, account_Password, account_City, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+
+// datatypes :- number - 2^53 , string, bigInt, boolean, 
+// null, undefined
+
+let my_name = "nihal"
+let age = 22
+let isLoggedIn = false
+let state = null;
+let contact;
+
+console.table([age, my_name, isLoggedIn, state , contact])
+console.log(typeof isLoggedIn) //boolean
+console.log(typeof age) //number
+console.log(typeof state) //object
+console.log(typeof contact) //undefined
