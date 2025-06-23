2fd481129915a46af49090a2bd720ee89fdbb7f5
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-06-23 15:41:26 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,52 @@
+//Primitive - String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//all primitive data types are call by value => copy of the value is created when a primitve
+// datatype is passed as a parameter to a func
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+console.log(typeof outsideTemp); //returns object
+
+
+let userEmail; //undefined
+console.log(typeof userEmail); //returns undefined
+
+//Even if the values passed inside Symbol is same, they are different/unique
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId); //returns false, that means they aren't same
+
+
+//BigInt
+const bigNumber = 234565434564356754675n;
+console.log(bigNumber);
+console.log(typeof bigNumber); //bigint
+
+
+
+
+// Reference - array, objects, functions
+
+//reference to original object is passed, so any modification inside the func will affect
+// the object outside the function
+
+const heros = ['ironman', 'superman', 'batman', 666]
+console.log(typeof heros);
+
+
+
+let myObj = {
+    name : "nihal",
+    age : 22,
+}
+console.log(myObj);
+console.log(typeof myObj); //returns object
+
+const myFunction = function() {
+    console.log("Hello jii");
+}
+
+ console.log(typeof myFunction); //returns function
