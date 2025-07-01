436a37f169b6be3af9e52cc0c9695511487a8900
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-07-01 16:28:20 +0000

    scoping

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,45 @@
+// let a = 300
+
+// if (true) {
+//     let a = 30
+//     const b = 20
+//     console.log('Inner:', a);
+// }
+
+// console.log(a);
+
+
+
+//nested scope -> children functions can access variables of parent function
+//but vice-versa cannot happen
+
+function one() {
+    const username = 'nihal'
+
+    function two() {
+        const website = 'youtube'
+        console.log(username);
+    }
+
+    // console.log(website); //error -> can't access variable outside its scope
+
+    two()  
+}
+
+one()
+
+
+
+
+if (true) {
+    const username = 'nihal'
+    if (username === 'nihal') {
+        const website = 'youtube'
+        console.log(username + website);
+    }
+    // console.log(website); //error  
+}
+
+// console.log(username); //again returns error -> var username out of scope myann
+
+
