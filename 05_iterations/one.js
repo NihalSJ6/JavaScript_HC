570c1b1503ef135508ec1ba9737b3ab4f097e252
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-07-07 16:47:41 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,17 @@
+// for (let i=0; i<=6; i++) {
+//     console.log(`Outer loop value : ${i} `);
+    
+//     for(let j=0; j<=6; j++) {
+//         console.log(`Inner loop is ${j} and outer loop is ${i}`);
+//     }
+// }
+
+
+let myArray = ['flash', 'deamon targaryen', 'jon snow']
+console.log(myArray.length);
+
+for(let index=0; index <= myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+}
+
