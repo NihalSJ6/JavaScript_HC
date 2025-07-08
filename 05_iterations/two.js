0665b232ae16fbf904d71aea697dfc9dcc9a3d36
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-07-08 02:36:07 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+//while loop
+
+let myArray = ['flash', 'deamon targaryen', 'jon snow']
+
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+    arr = arr + 1;
+}
+
+
+let score = 11
+
+
+//do while
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
+//do gets executed irrespective of while atleast once
+
